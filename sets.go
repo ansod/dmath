1122f69fc8c530be08d7adf1016f84a73f08eebb
ansod <anton1.soderberg@gmail.com>
2017-11-10 19:06:07 +0100

    Added union operation

--- sets.go
+++ sets.go
@@ -1,6 +1,27 @@
 package dmath
 
 
-func Hello() string {
-    return "hello!"
+func Union(sets ...[]int) []int {
+    var s1 []int
+
+    if len(sets) > 0 {
+        for _, set := range sets {
+            for _, val := range set {
+                if !sliceContains(val, s1) {
+                    s1 = append(s1, val)
+                }
+            }
+        }
+    }
+    return s1
+}
+
+
+func sliceContains(i int, set []int) bool {
+    for _,v := range set {
+        if v == i {
+            return true
+        }
+    }
+    return false
 }
