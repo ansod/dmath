439b2e2941820f0bfed284c19f069c4c44070398
ansod <anton1.soderberg@gmail.com>
2017-11-10 23:13:31 +0100

    Added intersection operation

--- sets.go
+++ sets.go
@@ -2,23 +2,42 @@ package dmath
 
 
 func Union(sets ...[]int) []int {
-    var s1 []int
+    var s []int
 
     if len(sets) > 0 {
         for _, set := range sets {
             for _, val := range set {
-                if !sliceContains(val, s1) {
-                    s1 = append(s1, val)
+                if !sliceContains(val, s) {
+                    s = append(s, val)
                 }
             }
         }
     }
-    return s1
+    return s
 }
 
 
+func Intersection(sets ...[]int) []int {
+    var s []int
+
+    for _, set := range sets {
+        for _, val := range set {
+            occurrences := 0
+            for _, set2 := range sets {
+                if sliceContains(val, set2) && !sliceContains(val, s){
+                    occurrences++
+                }
+            }
+            if occurrences == len(sets) {
+                s = append(s, val)
+            }
+        }
+    }
+    return s
+}
+
 func sliceContains(i int, set []int) bool {
-    for _,v := range set {
+    for _, v := range set {
         if v == i {
             return true
         }
