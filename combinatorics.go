c51f9434c50a38629fcc8c89e5a03b4fec4ffbb7
ansod <anton1.soderberg@gmail.com>
2017-11-13 00:03:27 +0100

    Added combinations & permutations

--- combinatorics.go
+++ combinatorics.go
@@ -13,3 +13,16 @@ func Factorial(n int64) *big.Int {
     x := big.NewInt(1)
     return x.MulRange(1, n)
 }
+
+func Combination(n, k int64) *big.Int {
+    x := big.NewInt(1)
+    y := big.NewInt(1)
+
+    return x.Div(Factorial(n), y.Mul(Factorial(k), Factorial(n-k)))
+}
+
+func Permutation(n, k int64) *big.Int {
+    x := big.NewInt(1)
+
+    return x.Div(Factorial(n), Factorial(n-k))
+}
