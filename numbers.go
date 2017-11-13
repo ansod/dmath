be3dcb6128ef21706014291e9093b644318bf7b2
ansod <anton1.soderberg@gmail.com>
2017-11-13 01:03:11 +0100

    Added gcd

--- numbers.go
+++ numbers.go
@@ -13,3 +13,18 @@ func IsPrime(n int) bool {
     }
     return true
 }
+
+// Returns the greatest common devisor
+// of two ints x and y
+func Gcd(x, y int) int {
+    r := x % y
+    c := x
+
+    for r != 0 {
+        c = r
+        r = y % r
+        y = c
+    }
+
+    return c
+}
